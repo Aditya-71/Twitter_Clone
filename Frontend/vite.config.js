9fe4ec31587b979b0b4c393dea3ae37a8bf8ad9e
Aditya Dubey <dubeyaditya196@gmail.com>
2024-08-09 19:19:19 +0530

    resolve merge confilict

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,11 +10,7 @@ export default defineConfig({
     // Get rid of the CORS error
     proxy: {
       "/api": {
-<<<<<<< HEAD
         target: process.env.VITE_BASE_URL ,
-=======
-        target:import.meta.env.VITE_BASE_URL ,
->>>>>>> 986a6e6898b1cc591487ad2ef4edc8e70af772b6
         changeOrigin: true,
         secure: false, // because it is not https , it is http
       },
