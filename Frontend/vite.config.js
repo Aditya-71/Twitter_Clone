5a8f2700781f5d831bd0ae82a2107a93e366b63c
Aditya-71 <121573550+Aditya-71@users.noreply.github.com>
2024-08-09 19:48:07 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // console.log('VITE_BASE_URL:', import.meta.env.BASE_URL);
 // https://vitejs.dev/config/
