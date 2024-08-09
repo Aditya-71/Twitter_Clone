a7e6981cfa3ccdd93fa3d4c98a64027131671a3a
Aditya Dubey <dubeyaditya196@gmail.com>
2024-08-09 12:33:42 +0530

    update bcrypt to bcryptjs

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import Post from "../models/postModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 import mongoose from "mongoose";
 import { v2 as cloudinary } from "cloudinary";
