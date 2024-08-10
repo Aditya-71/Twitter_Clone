a8ff8cc266024c66e104b0231fdf5b232e797e92
Aditya-71 <121573550+Aditya-71@users.noreply.github.com>
2024-08-10 11:10:16 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -38,11 +38,11 @@ app.use("/api/messages", messageRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 	// react app
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(process.cwd(), "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
 	});
 }
 
